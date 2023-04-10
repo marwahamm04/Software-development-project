e6a03b55f92567251853f7289fb92b855f763edb
marwahamm04 <112507192+marwahamm04@users.noreply.github.com>
2023-04-10 17:47:07 -0400

    Update final.py

--- final.py
+++ final.py
@@ -8,7 +8,7 @@ Output(s):
 c = 0
 
 
-def coin_Calc(c, quarter, dime, nickle, penny):
+def coin_Calc(c, quarter, dime, nickel, penny):
     if c >= 0:
         if c >= 25:
             quarter = c // 25
@@ -17,13 +17,13 @@ def coin_Calc(c, quarter, dime, nickle, penny):
             dime = c // 10
             c = c - 10 * dime
         if c >= 5:
-            nickle = c //5
-            c = c - 5 * nickle
+            nickel = c //5
+            c = c - 5 * nickel
         else:
             penny = c // 1
     else:
         c = 0
-    return quarter, dime, nickle, penny
+    return quarter, dime, nickel, penny
       
       
 
@@ -31,7 +31,7 @@ def main():
     user = "y"
     quarter = 0
     dime = 0
-    nickle = 0
+    nickel = 0
     penny = 0
 
     print("Coin Return Calculator")
@@ -45,11 +45,11 @@ def main():
                 if c <= 0:
                     raise ValueError("Error! Invalid interger entered please try again.")
 
-                q, d, n, p = coin_Calc(c, quarter, dime, nickle, penny)
+                q, d, n, p = coin_Calc(c, quarter, dime, nickel, penny)
         
                 print(f'{q} quarter(s)')
                 print(f'{d} dime(s)')
-                print(f'{n} nickle(s)')
+                print(f'{n} nickel(s)')
                 print(f'{p} penny(ies)')
 
         
