0e5444cc7d933b9405ec67961effd7676fa497fc
marwahamm04 <112507192+marwahamm04@users.noreply.github.com>
2023-04-13 17:15:41 -0400

    Update finaltest.py

--- finaltest.py
+++ finaltest.py
@@ -3,11 +3,17 @@ from final import coin_Calc
 
 class coin_CalcTest(unittest.TestCase):
 
-    def test_if_it_is_positive_number(self):
+    def test_if_it_there_is_out_come_(self):
         
         result = coin_Calc(7,0,0,0,0) 
         
         self.assertEqual(result, (0, 0, 1, 2))
+
+    def test_if_it_eight_gives_out_come_(self):
+        
+        result = coin_Calc(8,0,0,0,0) 
+        
+        self.assertEqual(result, (0, 0, 1, 3))
 
 if __name__ == '__main__':
    unittest.main()
